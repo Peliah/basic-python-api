50280bad6fe36729154487eb44c4868dd4d856e4
Peliah <pelepoupa@gmail.com>
2025-01-31 01:31:42 +0100

    tried changes with endpoint

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
 from datetime import datetime
 from flask_cors import CORS
+from flask_caching import Cache
 
 app = Flask(__name__)
 CORS(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/', methods=['GET'])
+@cache.cached(timeout=60)
 def get_info():
 
     data = {
